8cb773076609e6885b0d9d6c6df8583c417e2f22
Somanath Chowdary <somanath.chowdary@ymail.com>
2020-05-28 15:53:05 +0530

    Implement drinks delete endpoing

--- backend/src/api.py
+++ backend/src/api.py
@@ -152,6 +152,24 @@ def patch_drink(payload, drink_id):
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<int:drink_id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(payload, drink_id):
+    drink = Drink.query.get(drink_id)
+
+    if not drink:
+        abort(404)
+
+    try:
+        drink.delete()
+        return jsonify({
+            'success': True,
+            'delete': drink_id
+        })
+    except Exception as e:
+        # Print statement for debugging
+        print(e)
+        abort(422)
 
 
 ## Error Handling
